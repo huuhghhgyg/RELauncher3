a307ca2cfbe3b55b840229408dfda89de68db7da
huuhghhgyg <1657936673@qq.com>
2018-02-05 21:59:14 +0800

    问题修复

--- RELauncher3/Launcher/MCVersion.xaml.cs
+++ RELauncher3/Launcher/MCVersion.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RELauncher3.Launcher
 {
@@ -26,6 +18,42 @@ namespace RELauncher3.Launcher
             InitializeComponent();
             DataBase db = new DataBase();
             VersionJson = db.GetMinecraftVersionJson(1);
+            LoadInfo();
+        }
+
+        void LoadInfo()
+        {
+            //填入信息
+            var versions = App.Core.GetVersions().ToArray();
+            ListBoxVersion.ItemsSource = versions;
+            ListBoxVersion.DisplayMemberPath = "Id";
+
+            //自动选择首项
+            if (ListBoxVersion.Items.Count != 0)
+            {
+                ListBoxVersion.SelectedIndex = 0;
+                FillInfo();
+            }
+            else
+            {
+
+            }
+        }
+
+        void FillInfo()
+        {
+            //!!!///获取id方法
+            KMCCC.Launcher.Version ver = new KMCCC.Launcher.Version();
+            ver = ListBoxVersion.SelectedItem as KMCCC.Launcher.Version;
+            VersionBlock.Text = ver.Id;
+            //!!!///
+            string path = Directory.GetCurrentDirectory() + "\\.minecraft\\versions\\" + VersionBlock.Text;
+            if (Directory.Exists(path) == true)
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                CreateTimeBlock.Text = dir.CreationTimeUtc.ToLongDateString();
+            }
+            PathBlock.Text = path;
         }
 
         private void BackTile_Click(object sender, RoutedEventArgs e)
@@ -49,6 +77,45 @@ namespace RELauncher3.Launcher
                     }
                 }
             }
+        }
+
+        private void ListBoxVersion_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            FillInfo();
+        }
+
+        private void OpenPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(Directory.Exists(PathBlock.Text))
+            {
+                Process.Start(PathBlock.Text);
+            }
+        }
+
+        private void TurnToLaunchButton_Click(object sender, RoutedEventArgs e)
+        {
+            Grid grid;
+            grid = new LaunchVerify();
+            showGrid.Children.Clear();
+            showGrid.Children.Add(grid);
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Directory.Exists(PathBlock.Text))
+            {
+                if (System.Windows.MessageBox.Show("您确定要删除吗？", "提示：", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                {
+                    //delete
+                    Directory.Delete(PathBlock.Text, true);
+                    LoadInfo();
+                }
+            }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadInfo();
         }
     }
 }
